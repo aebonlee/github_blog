327cc72d4380b275431b7aa2d56a8e5b084d7f5a
aebon <98076341+aebonlee@users.noreply.github.com>
2024-05-17 15:58:02 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+const siteConfig = {
+    username: "paullabkorea", // GitHub 사용자 이름
+    repositoryName: "github_blog", // GitHub 저장소 이름
+    mainColor: "#3498db", // 사이트의 주 색상
+    textColor: "#333333", // 기본 텍스트 색상
+    blogTitle: "" // 블로그 제목
+};
